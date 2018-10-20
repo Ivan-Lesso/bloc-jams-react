976d8488f856a6488400f94c41d8ea88c24e406a
Ivan Zalessov <ivan.zalessov@gmail.com>
2018-10-20 16:55:19 -0500

    Albums Assignment

--- src/components/Album.js
+++ src/components/Album.js
@@ -31,6 +31,15 @@ class Album extends Component {
             <col id="song-duration-column" />
           </colgroup>
           <tbody>
+          {
+            this.state.album.songs.map((song, index) =>
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{song.title}</td>
+              <td>{(song.duration-(song.duration%=60))/60+(9<song.duration?':':':0')+Math.ceil(song.duration)}</td>
+            </tr>
+          )
+          }
           </tbody>
         </table>
       </section>
